562c9c1f29e165fee0d8337053063a7bcb63827d
Abimaelr <abimaelr.albu@gmail.com>
2021-10-12 17:22:28 -0300

    Sass

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import {
 } from "react-router-dom";
 import Home from "./Screens/Home";
 import Login from "./Screens/Login";
+import Turmas from "./Screens/Turmas";
+import Nav from "./Components/Nav"
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Nav />
         <Switch>
           <Route exact path="/login" component={Login} />
+          <Route exact path="/turmas" component={Turmas} />
           <Route exact path="/" component={Home} />
         </Switch>
       </Router>
