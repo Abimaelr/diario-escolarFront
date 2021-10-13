10eef56bfcd9e2eb4a683d15ff9375b2f6f87e3b
Abimaelr <abimaelr.albu@gmail.com>
2021-10-13 12:38:27 -0300

    Painel de frequencias alunos

--- src/Screens/Diarios.jsx
+++ src/Screens/Diarios.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { Container } from 'react-bootstrap';
 import api from '../Api/Axios';
-
+import './css/Diarios.css'
 function Diarios() {
 
     const [action, setAction] = useState("inserir");
@@ -10,11 +10,17 @@ function Diarios() {
     const [turmas, setTurmas] = useState([]);
     const [disciplina, setDisciplina] = useState("");
     const [disciplinas, setDisciplinas] = useState([]);
+    const [bimestre, setBimestre] = useState("");
+    const [students, setStudents] = useState([]);
 
     useEffect(() => {
         api.get('/disciplinas').then(({data}) => setDisciplinas(data.disciplinas)).catch(({response}) => alert(response))
         api.get('/classes/p').then(({data}) => setTurmas(data.classes)).catch(({response}) => alert(response))
     }, [])
+
+    useEffect(()=> {
+        api.get(`/classes/students/${turma}`).then(({data}) => setStudents(data.students)).catch(({response}) => alert(response))
+    },[turma])
 
     return (
         <div>
@@ -39,8 +45,33 @@ function Diarios() {
                             {disciplinas.map((e,i) => <option key={ i } value={ e }>{ e }</option>)}
                         </select>
                         <input type="date" id="date"  onChange={({ target }) => { setData(target.value) }}/>
+                        <select name="bimestre" id="bimestre"  onChange={({ target }) => {setBimestre(target.value)}}>
+                            <option value="-">Escolha o bimestre</option>
+                            <option value="1">1°</option>
+                            <option value="2">2°</option>
+                            <option value="3">3°</option>
+                            <option value="4">4°</option>
+                        </select>
                     </form>
                 </header>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Matrícula</th>
+                                <th>Presença</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { students.map((s,i) => <tr key={i}>
+                                <td>{s.nomeCompleto}</td>
+                                <td>{s.alunoId}</td>
+                                <td><input type="checkbox" id={ s.alunoId } checked/></td>
+                            </tr>) }
+                        </tbody>
+                    </table>
+                </div>
             </Container>
         </div>
     )
