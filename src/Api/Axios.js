1d824ff0ed8a239ad6a8fb20f5155c714d758a56
Abimael <abimaelr.albu@gmail.com>
2021-11-12 09:49:05 -0300

    Diario

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "http://diario-escolar-backend.herokuapp.com/"
 });
 
 api.defaults.headers.common['Authorization'] = localStorage.getItem('token');
